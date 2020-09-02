15888d401f0e50e52ff2108043ce518ec146401b
Ben Roberts <benjamindaveroberts@gmail.com>
2020-09-02 17:40:51 +0100

    Added Breadth First Search
    Also added javadoc to much of the project

--- src/main/java/AISearch.java
+++ src/main/java/AISearch.java
@@ -3,35 +3,42 @@ import java.util.Collections;
 
 public class AISearch
 {
-
-    private static AbstractSearchNode startNode;
-    private static int solutionDepth = -1;
-
+    /**
+     * Applies depth first search on the node given to it.
+     * Any nodes that satisfy the node equality function on nodes that have already been expanded are skipped.
+     * @param originNode The root node of the depth first search.
+     * @return The first node found that matches the goal state.
+     * @throws Exception If no node is found that matches the goal state then an exception is thrown.
+     */
     public static AbstractSearchNode applyDepthFirstSearch(AbstractSearchNode originNode) throws Exception
     {
-        return applyDepthFirstSearch(originNode, false);
+        return applyDepthFirstSearch(originNode, true);
     }
 
-
+    /**
+     * Applies depth first search on the node given to it.
+     * @param originNode The root node of the depth first search.
+     * @param pruneExpandedNodes If this flag is set then then any nodes that satisfy the node equality function on nodes that have already been expanded are skipped.
+     * @return The first node found that matches the goal state.
+     * @throws Exception If no node is found that matches the goal state then an exception is thrown.
+     */
     public static AbstractSearchNode applyDepthFirstSearch(AbstractSearchNode originNode, boolean pruneExpandedNodes) throws Exception
     {
-        startNode = originNode;
-        solutionDepth = 0;
-        if (startNode.inGoalState())
+        originNode.setNodeDepth(0);
+        if (originNode.inGoalState())
         {
-            return startNode;
+            return originNode;
         }
         ArrayList<AbstractSearchNode> fringe = new ArrayList<>();
-        ArrayList<AbstractSearchNode> nodesExpanded = new ArrayList<>();
-        fringe.add(startNode);
+        ArrayList<AbstractSearchNode> expandedNodes = new ArrayList<>();
+        fringe.add(originNode);
         AbstractSearchNode currentNode;
         while (!fringe.isEmpty())
         {
             currentNode = fringe.remove(fringe.size() - 1);
-            solutionDepth++;
             if (pruneExpandedNodes)
             {
-                nodesExpanded.add(currentNode);
+                expandedNodes.add(currentNode);
             }
             ArrayList<AbstractSearchNode> newNodes = currentNode.generateChildNodes();
             Collections.shuffle(newNodes);
@@ -39,13 +46,12 @@ public class AISearch
             {
                 if (node.inGoalState())
                 {
-                    solutionDepth++;
                     return node;
                 }
                 boolean addToFringe = true;
                 if (pruneExpandedNodes)
                 {
-                    for (AbstractSearchNode exNode : nodesExpanded)
+                    for (AbstractSearchNode exNode : expandedNodes)
                     {
                         if (node.equalsNode(exNode))
                         {
@@ -63,14 +69,68 @@ public class AISearch
         throw new Exception("No solution found at any depth");
     }
 
-    public static AbstractSearchNode applyBreadthFirstSearch(AbstractSearchNode originNode) //TODO
+    /**
+     * Applies breadth first search on the node given to it.
+     * Any nodes that satisfy the node equality function on nodes that have already been expanded are skipped.
+     * @param originNode The root node of the breadth first search.
+     * @return The first node found that matches the goal state.
+     * @throws Exception If no node is found that matches the goal state an exception is thrown.
+     */
+    public static AbstractSearchNode applyBreadthFirstSearch(AbstractSearchNode originNode) throws Exception
     {
-        startNode = originNode;
-        if (startNode.inGoalState())
+        return applyBreadthFirstSearch(originNode, true);
+    }
+
+    /**
+     * Applies breadth first search on the node given to it.
+     * @param originNode The root node of the breadth first search.
+     * @param pruneExpandedNodes If this flag is set then then any nodes that satisfy the node equality function on nodes that have already been expanded are skipped.
+     * @return The first node found that matches the goal state.
+     * @throws Exception If no node is found that matches the goal state an exception is thrown.
+     */
+    public static AbstractSearchNode applyBreadthFirstSearch(AbstractSearchNode originNode, boolean pruneExpandedNodes) throws Exception
+    {
+        originNode.setNodeDepth(0);
+        if (originNode.inGoalState())
         {
-            solutionDepth = 0;
-            return startNode;
+            return originNode;
         }
-        return originNode;
+        ArrayList<AbstractSearchNode> fringe = new ArrayList<>();
+        ArrayList<AbstractSearchNode> expandedNodes = new ArrayList<>();
+        fringe.add(originNode);
+        AbstractSearchNode currentNode;
+        while(!fringe.isEmpty())
+        {
+            currentNode = fringe.get(0);
+            if (pruneExpandedNodes)
+            {
+                expandedNodes.add(currentNode);
+            }
+            ArrayList<AbstractSearchNode> newNodes = currentNode.generateChildNodes();
+            for (AbstractSearchNode node : newNodes)
+            {
+                if (node.inGoalState())
+                {
+                    return node;
+                }
+                boolean addToFringe = true;
+                if (pruneExpandedNodes)
+                {
+                    for (AbstractSearchNode exNode : expandedNodes)
+                    {
+                        if (node.equalsNode(exNode))
+                        {
+                            addToFringe = false;
+                            break;
+                        }
+                    }
+                }
+                if (addToFringe)
+                {
+                    fringe.add(node);
+                }
+            }
+        }
+        throw new Exception("No solution found at any depth");
     }
 }
--- src/main/java/AbstractSearchNode.java
+++ src/main/java/AbstractSearchNode.java
@@ -3,23 +3,44 @@ import java.util.ArrayList;
 public abstract class AbstractSearchNode
 {
 	private AbstractSearchNode parentNode;
+	private int nodeDepth;
 
 
+	/**
+	 * Constructs a new ASN. This constructor should be used when creating a root node.
+	 */
 	public AbstractSearchNode()
 	{
 		this.parentNode = null;
+		nodeDepth = 0;
 	}
 
+	/**
+	 * Constructs a new ASN. This constructor should be used when creating a child node.
+	 * @param parentNode The parent of the new node.
+	 */
 	public AbstractSearchNode(AbstractSearchNode parentNode)
 	{
 		this.parentNode = parentNode;
+		this.nodeDepth = this.parentNode.getNodeDepth() + 1;
+	}
+
+	public int getNodeDepth()
+	{
+		return nodeDepth;
+	}
+
+	public void setNodeDepth(int nodeDepth)
+	{
+		this.nodeDepth = nodeDepth;
 	}
 
 	public abstract boolean inGoalState();
 
 	/**
-	 * This method is used to set the children of the current node. Any subclass should set the variable childNodes
-	 * in their implementation of this method.
+	 * This method is used to set the children of the current node.
+	 *
+	 * @return The child nodes of the current node.
 	 */
 	public abstract ArrayList<AbstractSearchNode> generateChildNodes();
 
